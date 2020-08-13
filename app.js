ccbd43650326b35a442d705bd50152cd1d295528
Jia <pochialu@gmail.com>
2020-08-13 14:39:48 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
+
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
